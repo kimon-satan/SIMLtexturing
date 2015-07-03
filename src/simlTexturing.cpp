eb2b770fbfad72911da91a03af9f38c639531b79
kimon-satan <simon@simonkatan.co.uk>
2015-07-03 11:51:26 +0100

    render to FBO

--- src/simlTexturing.cpp
+++ src/simlTexturing.cpp
@@ -3,6 +3,7 @@
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Vbo.h"
+#include "cinder/gl/Fbo.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/ImageIo.h"
 
@@ -12,20 +13,22 @@ using namespace ci::app;
 using std::vector;
 
 
-/*** This sample demonstrates the Vbo class by creating a simple grid mesh with a texture mapped onto it.
- * The mesh has static indices and texture coordinates, but its vertex positions are dynamic.
- * It also creates a second mesh which shares static and index buffers, but has its own dynamic buffer ***/
-
 class SimlTexturingApp : public AppBasic {
+    
  public:
 	void setup();
 	void update();
 	void draw();
+    void renderSceneToFbo();
 
-	static const int VERTICES_X = 6, VERTICES_Y = 2;
+    static const int NUM_SCREENS = 6;
+	int mVerticesX, mVerticesY;
 
+    gl::Fbo mFbo;
 	gl::VboMeshRef	mVboMesh;
 	gl::TextureRef	mTexture;
+    
+    static const int	FBO_WIDTH = 1200, FBO_HEIGHT = 67;
     
     void prepareSettings(Settings * settings);
     
@@ -36,92 +39,126 @@ class SimlTexturingApp : public AppBasic {
 
 };
 
+void SimlTexturingApp::prepareSettings(Settings * settings){
+    settings->setWindowSize(128 * 6, 72);
+    settings->setFrameRate( 60.0f );
+}
+
 void SimlTexturingApp::setup()
 {
 
+
+    gl::Fbo::Format format;
+    //format.setSamples( 4 ); // uncomment this to enable 4x antialiasing
+    mFbo = gl::Fbo( FBO_WIDTH, FBO_HEIGHT, format );
     
+    mVerticesX = NUM_SCREENS * 2;
+    mVerticesY = 2;
 	// setup the parameters of the Vbo
-	int totalVertices = VERTICES_X * VERTICES_Y;
-	int totalQuads = ( VERTICES_X - 1 ) * ( VERTICES_Y - 1 );
+	int totalVertices = mVerticesX * mVerticesY;
+	int totalQuads = ( mVerticesX - 1 ) * ( mVerticesY - 1 );
 	gl::VboMesh::Layout layout;
 	layout.setStaticIndices();
 	layout.setStaticPositions();
 	layout.setStaticTexCoords2d();
 	mVboMesh = gl::VboMesh::create( totalVertices, totalQuads * 4, layout, GL_QUADS );
 
+
 	
 	// buffer our static data - the texcoords and the indices
 	vector<uint32_t> indices;
 	vector<Vec2f> texCoords;
     
-    float  x_coords[6] = {0.0, 0.25, 0.25, 0.75, 0.75, 1.0};
-    float y_coords [12] = {
-        0.0 ,0.5,
-        0.0, 0.5,
-        0.0, 1.0,
-        0.0 ,1.0,
-        0.5, 1.0,
-        0.5, 1.0
-    };
+    vector<float> winXPoints(NUM_SCREENS * 2);
+    vector<float> texXPoints(NUM_SCREENS * 2);
+    vector<float> winYPoints(NUM_SCREENS * 4);
+    vector<float> texYPoints(NUM_SCREENS * 4);
     
-    float  x_tcoords[6] = {0.0, 0.33, 0.33, 0.66, 0.66, 1.0};
-    float y_tcoords [12] = {
-        0.0 ,1.0,
-        0.0, 1.0,
-        0.0, 1.0,
-        0.0 ,1.0,
-        0.0, 1.0,
-        0.0, 1.0
-    };
-    
-	for( int x = 0; x < VERTICES_X; ++x ) {
-		for( int y = 0; y < VERTICES_Y; ++y ) {
-			// create a quad for each vertex, except for along the bottom and right edges
-			if( ( x + 1 < VERTICES_X ) && ( y + 1 < VERTICES_Y ) ) {
-				indices.push_back( (x+0) * VERTICES_Y + (y+0) );
-                
-				indices.push_back( (x+1) * VERTICES_Y + (y+0) );
-				indices.push_back( (x+1) * VERTICES_Y + (y+1) );
-				indices.push_back( (x+0) * VERTICES_Y + (y+1) );
-			}
-			// the texture coordinates are mapped to [0,1.0)
-			texCoords.push_back(Vec2f(x_tcoords[x], y_tcoords[y]));
-		}
-	}
-	
-	mVboMesh->bufferIndices( indices );
-	mVboMesh->bufferTexCoords2d( 0, texCoords );
-	
-	mTexture = gl::Texture::create( loadImage( loadResource( RES_IMAGE ) ) );
-    
-    // dynmaically generate our new positions based on a simple sine wave
-    
-    
-    vector<Vec3f> positions;
-    positions.resize(totalVertices);
-    
+    float incr = 1.0/(float)NUM_SCREENS;
 
-    //mVboMesh->
-    for( int x = 0; x < VERTICES_X; ++x ) {
-        for( int y = 0; y < VERTICES_Y; ++y ) {
-            Vec3f p(  x_coords[x],  y_coords[y + x * 2], 0 );
-            positions[x * VERTICES_Y + y] = p;
+    for(int i = 0; i < winXPoints.size(); i++)
+    {
+        if(i%2 == 0){
+          winXPoints[i] = (i/2) * incr;
+          texXPoints[i] = (i/2) * incr;
+        }else{
+          winXPoints[i] = ((i+1)/2) * incr;
+          texXPoints[i] = ((i+1)/2) * incr;
         }
     }
     
-    mVboMesh->bufferPositions(positions);
+    for(int i = 0; i < winYPoints.size(); i++)
+    {
+        winYPoints[i] = (i%2 == 0)? 0.0 : 1.0;
+        texYPoints[i] = (i%2 == 0)? 0.0 : 1.0;
+    }
+
+    vector<Vec3f> positions;
+    positions.resize(totalVertices);
     
-    /*iter = mVboMesh->mapVertexBuffer();
-    for( int i = 0; i < totalVertices; ++i ) {
-            Vec3f p = *iter.getPositionPointer();
-            iter.setPosition( Vec3f(p.x,  p.y,  i *   ) );
-            ++iter;
-    }*/
+	for( int x = 0; x < mVerticesX; ++x ) {
+		for( int y = 0; y < mVerticesY; ++y ) {
+			// create a quad for each vertex, except for along the bottom and right edges
+			if( ( x + 1 < mVerticesX ) && ( y + 1 < mVerticesY ) ) {
+                
+				indices.push_back( (x+0) * mVerticesY + (y+0) );
+				indices.push_back( (x+1) * mVerticesY + (y+0) );
+				indices.push_back( (x+1) * mVerticesY + (y+1) );
+				indices.push_back( (x+0) * mVerticesY + (y+1) );
+			}
+			// the texture coordinates are mapped to [0,1.0)
+			texCoords.push_back(Vec2f(texXPoints[x], texYPoints[y]));
+            Vec3f p(  winXPoints[x],  winYPoints[y + x * 2], 0 );
+            positions[x * mVerticesY + y] = p;
+		}
+	}
+	
+
+    
+	mVboMesh->bufferIndices( indices );
+	mVboMesh->bufferTexCoords2d( 0, texCoords );
+    mVboMesh->bufferPositions(positions);
+    mVboMesh->unbindBuffers();
+    
+	mTexture = gl::Texture::create( loadImage( loadResource( RES_IMAGE ) ) );
+    
+    renderSceneToFbo();
+
 }
 
-void SimlTexturingApp::prepareSettings(Settings * settings){
-    settings->setWindowSize(1280, 300);
+void SimlTexturingApp::renderSceneToFbo()
+{
+    // this will restore the old framebuffer binding when we leave this function
+    // on non-OpenGL ES platforms, you can just call mFbo.unbindFramebuffer() at the end of the function
+    // but this will restore the "screen" FBO on OpenGL ES, and does the right thing on both platforms
+    gl::SaveFramebufferBinding bindingSaver;
+    
+    // bind the framebuffer - now everything we draw will go there
+    mFbo.bindFramebuffer();
+    
+    // setup the viewport to match the dimensions of the FBO
+    gl::setViewport( mFbo.getBounds() );
+    gl::setMatricesWindow( mFbo.getSize() );
+    
+    // set the modelview matrix to reflect our current rotation
+    
+    // clear out the FBO with blue
+    gl::clear( Color( 0.25, 0.5f, 1.0f ) );
+    
+    // render an orange torus, with no textures
+
+    gl::color( Color( 1.0f, 0.0f, 0.0f ) );
+    
+    float incr = mFbo.getWidth() /(float) NUM_SCREENS;
+    
+    for(int i = 0; i < NUM_SCREENS; i++){
+        gl::drawLine(Vec2f( i * incr,0), Vec2f( (i + 1) * incr, mFbo.getHeight()));
+    }
+    
+    mFbo.unbindFramebuffer();
+  
 }
+
 
 void SimlTexturingApp::update()
 {
@@ -137,12 +174,21 @@ void SimlTexturingApp::draw()
     gl::setMatricesWindow( getWindowSize() );
 	// this pair of lines is the standard way to clear the screen in OpenGL
 	gl::clear( Color( 0.15f, 0.15f, 0.15f ) );
-
-
+    
+    gl::color( Color( 1.0f, 1.0f, 1.0f ) );
+    
+    gl::pushMatrices();
     glScalef(getWindowWidth(), getWindowHeight(), 1.0);
-	mTexture->enableAndBind();
-	gl::draw( mVboMesh );
-	//gl::draw( mVboMesh2 );
+
+    mFbo.getTexture().enableAndBind();
+    gl::draw( mVboMesh );
+    mFbo.getTexture().unbind();
+    gl::popMatrices();
+
+
+    
+
+
 }
 
 
--- xcode/Build/Intermediates/PrecompiledHeaders/VBOSample_Prefix-abwhdxtzucznbvedbkwkuaivukqr/VBOSample_Prefix.pch.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/kimonsatan/code/cinder_0.8.6_mac/myApps/VBOSample/xcode/VBOSample_Prefix.pch
